9d46ecadaa65b303029e35d63f895ae4c163ab24
Jack Van <80840796+jackvan20000312@users.noreply.github.com>
2021-03-24 15:47:12 -0400

    Add files via upload

---
+++ Project00.java
@@ -0,0 +1,37 @@
+package fhj;
+//Author: Jack Van-Jacob Vaught
+//Project 00: My Name
+//This Program Displays Text!
+//Due on 8-26-2020
+import java.util.Scanner;  // Import Scanner class?
+
+
+public class Project00 {
+
+	public static void main(String[] args) {
+	
+		Scanner YourName = new Scanner(System.in);  //Input your name.
+	    System.out.println("Please Enter Your Name...");
+
+	    String Name = YourName.nextLine();  //Send YourName To a string.
+	    YourName.close();
+	    
+		System.out.println("Hello " + Name);//Output Name. Display Line 0
+		System.out.println("My Name Is Jacob Vaught");//Type display text here Hello world etc. Display Line 1
+		System.out.println("");//Type display text here Hello world etc. Display Line 2
+		System.out.println("");//Type display text here Hello world etc. Display Line 3
+		System.out.println("My Major is Electrical Engineering");//Type display text here Hello world etc. Display Line 4
+		System.out.println("");//Type display text here Hello world etc. Display Line 5
+		System.out.println("");//Type display text here Hello world etc. Display Line 6
+		System.out.println("jacobvaught07@gmail.com");//Type display text here Hello world etc. Display Line 7
+		System.out.println("");//Type display text here Hello world etc. Display Line 8
+		System.out.println("");//Type display text here Hello world etc. Display Line 9
+		System.out.println("vaughttechnologydiy@gmail.com");//Type display text here Hello world etc. Display Line 10
+		System.out.println("");//Type display text here Hello world etc. Display Line 11
+		System.out.println("");//Type display text here Hello world etc. Display Line 12
+		System.out.println("This was edited on 8-25-2020");//Type display text here Hello world etc. Display Line 13
+		System.out.println("Project Number 00");//Type display text here Hello world etc. Display Line 14
+		
+	}// Ending Bracket of STatic VOID
+	
+}//Ending Bracket of HelloWorld 
